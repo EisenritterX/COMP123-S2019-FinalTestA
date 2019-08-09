32a38154f57bb2989b9300765d066bcd8ef0d2f4
George Zhou <georgezhou89@hotmail.com>
2019-08-09 10:29:39 -0400

    Template Ver 1.0

---
+++ COMP123-S2019-FinalTestA/Views/HeroGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+/*
+ * STUDENT NAME: QIAN LONG (GEORGE) ZHOU
+ * STUDENT ID: 300613283
+ * DESCRIPTION: This is the Hero Generator Form
+ */
+namespace COMP123_S2019_FinalTestA.Views
+{
+    public partial class HeroGenerator : COMP123_S2019_FinalTestA.Views.MasterForm
+    {
+        public HeroGenerator()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the event handler for the Next Button Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex < MainTabControl.TabPages.Count - 1)
+            {
+                MainTabControl.SelectedIndex++;
+            }
+        }
+
+        /// <summary>
+        /// This is the event handler for the Back Button Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+
+        }
+    }
+}
