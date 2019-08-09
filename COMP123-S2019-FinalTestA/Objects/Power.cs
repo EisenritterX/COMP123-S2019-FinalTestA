02318f51a08970f7514f40d279dc6ec6d01da369
George Zhou <georgezhou89@hotmail.com>
2019-08-09 09:06:32 -0400

    Base Project

---
+++ COMP123-S2019-FinalTestA/Objects/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME: QIAN LONG (GEORGE) ZHOU
+ * STUDENT ID: 300613283
+ * DESCRIPTION: This is the Power Class
+ */
+namespace COMP123_S2019_FinalTestA.Objects
+{
+    class Power
+    {
+        // Public Properties
+        public string PowerName { get; set; }
+        public int Number { get; set; }
+    }
+}
--- COMP123-S2019-FinalTestA/Program.cs
+++ COMP123-S2019-FinalTestA/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using COMP123_S2019_FinalTestA.Views;
 
 namespace COMP123_S2019_FinalTestA
 {
@@ -16,7 +17,7 @@ namespace COMP123_S2019_FinalTestA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run();
         }
     }
 }
